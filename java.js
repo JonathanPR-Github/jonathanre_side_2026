52153d9adb35eb91e1bf090e65598f8391210bde
Jonathan <jonathan.reppen@outlook.com>
2026-04-13 00:07:11 +0200

    update 2
    update 2

---
+++ java.js
@@ -0,0 +1,9 @@
+function ShowTab_1() {
+  document.getElementById('container_tab_1').style.display = 'block';
+  document.getElementById('container_tab_2').style.display = 'none';
+}
+
+function ShowTab_2() {
+  document.getElementById('container_tab_1').style.display = 'none';
+  document.getElementById('container_tab_2').style.display = 'block';
+}
