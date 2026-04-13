19bfbeb81a419d71914c35dfaa5de9d3a0f87501
Jonathan <jonathan.reppen@outlook.com>
2026-04-13 23:05:14 +0200

    update 3
    update 3

--- java.js
+++ java.js
@@ -1,9 +1,11 @@
 function ShowTab_1() {
   document.getElementById('container_tab_1').style.display = 'block';
   document.getElementById('container_tab_2').style.display = 'none';
+  document.body.style.height = '120vh'
 }
 
 function ShowTab_2() {
   document.getElementById('container_tab_1').style.display = 'none';
   document.getElementById('container_tab_2').style.display = 'block';
+  document.body.style.height = '250vh'
 }
