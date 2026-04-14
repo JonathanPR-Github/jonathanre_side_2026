1848cda1499f493a5f301709a345c3c3befbc84a
Jonathan <jonathan.reppen@outlook.com>
2026-04-15 00:37:07 +0200

    update 4
    update 4

--- java.js
+++ java.js
@@ -7,5 +7,19 @@ function ShowTab_1() {
 function ShowTab_2() {
   document.getElementById('container_tab_1').style.display = 'none';
   document.getElementById('container_tab_2').style.display = 'block';
-  document.body.style.height = '250vh'
+  document.body.style.height = '100vh'
+}
+
+function OpenPopup() {
+  document.getElementById('container_tab_popup').style.display = 'block';
+}
+function ClosePopup() {   // NOTAT: Leg til alle videre popup specific meldinger in i her
+  document.getElementById('container_tab_popup').style.display = 'none';
+
+  // Timeline functions for Popup
+  document.getElementById('container_popup_1').style.display = 'none';
+}
+
+function popup_1() {
+  document.getElementById('container_popup_1').style.display = 'flex';
 }
